d47379417e21a23496f44bdde0028327ea4bbdf7
Nalin Kumar <nalin12894+github@gmail.com>
2024-07-20 22:16:18 +0530

    added findByTitle

--- src/main/java/com/nk/blog/repo/BlogPostRepo.java
+++ src/main/java/com/nk/blog/repo/BlogPostRepo.java
@@ -1,5 +1,7 @@
 package com.nk.blog.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +12,6 @@ public interface BlogPostRepo extends JpaRepository<BlogPost, Long>{
     
 
 	Page<BlogPost> findAllByCreatedBy(Long userId,Pageable pageable);
+
+    Optional<BlogPost> findByTitle(String title);
 }
