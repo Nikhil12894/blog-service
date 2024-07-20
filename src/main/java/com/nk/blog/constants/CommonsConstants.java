8e6331b91d2f1eb0eee2d55710d0d8154ae91c75
Nalin Kumar <nalin12894+github@gmail.com>
2024-07-20 12:01:38 +0530

    added Constants used in app

---
+++ src/main/java/com/nk/blog/constants/CommonsConstants.java
@@ -0,0 +1,31 @@
+package com.nk.blog.constants;
+
+public abstract class CommonsConstants {
+
+	private CommonsConstants() {}
+	
+	public static final Long SYSTEM_USER = 0L;
+
+
+	public static final String DEFAULT_BLOG_STATUS = "DRAFT";
+	public static final String ALLOWED_BLOG_STATUS = "DRAFT,PUBLISHED,DELETED";
+
+
+	public static final Integer DEFAULT_PAGE = 1;
+
+    public static final Integer DEFAULT_PAGE_SIZE = 10;
+
+    public static final String DEFAULT_SORT_CREATED = "CREATED_AT";
+
+    public static final String DEFAULT_SORT_ORDER = "ASC";
+
+    public static final String PAGE = "page";
+
+    public static final String PAGE_SIZE = "page_size";
+
+    public static final String SORT_ORDER = "sort_order";
+
+    public static final String SORT_BY = "sort_by";
+
+    public static final String NO_SORT = "NONE";
+}
