71f066a76c13ccf6c0ab2aa2c8bcdb5ae5ab32e2
Nalin Kumar <nalin12894+github@gmail.com>
2024-07-21 10:42:25 +0530

    added unit test for findById, Update, and delete

--- src/test/java/com/nk/blog/service/BlogPostServiceTest.java
+++ src/test/java/com/nk/blog/service/BlogPostServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -50,6 +51,9 @@ class BlogPostServiceTest {
 
         }
 
+        /*
+         * ! ==========> Test for createBlog() <==========
+         */
         @Test
         void testCreateBlog_ValidRequest_ReturnsDTO() {
                 // Arrange
@@ -115,6 +119,10 @@ class BlogPostServiceTest {
                 // Act & Assert
                 assertThrows(CustomDataIntegrityViolationException.class, () -> service.createBlog(validRequest));
         }
+
+        /*
+         * ! ==========> Test for getAllBlogsCurrentUser() <==========
+         */
 
         @Test
         void testGetAllBlogsCurrentUser_ValidParameters_ReturnsBlogPostListDTO() {
@@ -258,11 +266,9 @@ class BlogPostServiceTest {
                 assertEquals(result, expectedResult);
         }
 
-
-
-
-        // Test for getAllBlogs()
-
+        /*
+         * ! ==========> Test for getAllBlogs() <==========
+         */
 
         @Test
         void testGetAllBlogs_ValidParameters_ReturnsBlogPostListDTO() {
@@ -299,7 +305,7 @@ class BlogPostServiceTest {
                                 .lastUpdatedBy(0L)
                                 .lastUpdatedAt(Testconstants.DEFAULT_DATETIME)
                                 .build()));
-                when(blogRepository.findAll( any(Pageable.class))).thenReturn(blogPage);
+                when(blogRepository.findAll(any(Pageable.class))).thenReturn(blogPage);
 
                 // Run the test
                 final BlogPostListDTO result = service.getAllBlogs(PostConstents.DEFAULT_PAGE,
@@ -309,7 +315,7 @@ class BlogPostServiceTest {
                 // Verify the results
                 assertEquals(result, expectedResult);
                 // Verify
-                verify(blogRepository, times(1)).findAll( any(Pageable.class));
+                verify(blogRepository, times(1)).findAll(any(Pageable.class));
         }
 
         @Test
@@ -375,7 +381,7 @@ class BlogPostServiceTest {
                 // Configure ScheduleRepo.findAll(...).
                 final Page<BlogPost> blogPage = new PageImpl<>(List.of(BlogPost.builder()
                                 .build()));
-                when(blogRepository.findAll( any(Pageable.class))).thenReturn(blogPage);
+                when(blogRepository.findAll(any(Pageable.class))).thenReturn(blogPage);
                 // Run the test
                 // Act & Assert
                 assertThrows(BadRequestException.class, () -> service.getAllBlogs(2,
@@ -406,32 +412,150 @@ class BlogPostServiceTest {
                 assertEquals(result, expectedResult);
         }
 
-        // Test for getBlog by id
+        /*
+         * ! ==========> Test for getBlogById() <==========
+         */
 
+        @Test
+        void testGetBlogById_WhenBlogPostExists_ThenReturnDTO() {
+                Long id = 1L;
+                BlogPost mockBlogPost = new BlogPost();
+                mockBlogPost.setId(id);
+                when(blogRepository.findById(id)).thenReturn(Optional.of(mockBlogPost));
 
-    @Test
-    void testGetBlogById_WhenBlogPostExists_ThenReturnDTO() {
-        Long id = 1L;
-        BlogPost mockBlogPost = new BlogPost();
-        mockBlogPost.setId(id);
-        when(blogRepository.findById(id)).thenReturn(Optional.of(mockBlogPost));
+                BlogPostDTO result = service.getBlogById(id);
 
-        BlogPostDTO result = service.getBlogById(id);
+                assertNotNull(result);
+                assertEquals(id, result.getId());
+                // Add more assertions based on your DTO structure
+        }
 
-        assertNotNull(result);
-        assertEquals(id, result.getId());
-        // Add more assertions based on your DTO structure
-    }
+        @Test
+        void testGetBlogById_WhenBlogPostDoesNotExist_ThenThrowException() {
+                Long id = 1L;
+                when(blogRepository.findById(id)).thenReturn(Optional.empty());
 
-    @Test
-    void testGetBlogById_WhenBlogPostDoesNotExist_ThenThrowException() {
-        Long id = 1L;
-        when(blogRepository.findById(id)).thenReturn(Optional.empty());
+                NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+                        service.getBlogById(id);
+                });
 
-        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
-            service.getBlogById(id);
-        });
+                assertTrue(exception.getMessage().contains("Blog Post not found"));
+        }
 
-        assertTrue(exception.getMessage().contains("Blog Post not found"));
-    }
+        /*
+         * ! ==========> Test for updateBlog() <==========
+         */
+        @Test
+        void testUpdateBlog_ValidRequest_ReturnsDTO() {
+                // Arrange
+                BlogPostRequest request = BlogPostRequest.builder()
+                                .title("Test Title")
+                                .id(1L)
+                                .description("Test Description")
+                                .content("Test Content")
+                                .imageUrl("Test Image URL")
+                                .status(BlogStatus.DRAFT)
+                                .build();
+                // Mock
+                when(blogRepository.findById(anyLong())).thenReturn(Optional.of(BlogPost.builder()
+                                .title("Test Title")
+                                .description("Test Description")
+                                .content("Test Content")
+                                .imageUrl("Test Image URL")
+                                .status(BlogStatus.DRAFT)
+                                .id(1L)
+                                .createdAt(LocalDateTime.now())
+                                .createdBy(PostConstents.SYSTEM_USER)
+                                .build()));
+                when(blogRepository.save(any(BlogPost.class))).thenReturn(BlogPost.builder()
+                                .title("Test Title")
+                                .description("Test Description")
+                                .content("Test Content")
+                                .imageUrl("Test Image URL")
+                                .status(BlogStatus.DRAFT)
+                                .id(1L)
+                                .createdAt(LocalDateTime.now())
+                                .createdBy(PostConstents.SYSTEM_USER)
+                                .build());
+                // Act
+                BlogPostDTO result = service.updateBlog(1L, request);
+
+                // Assert
+                assertEquals("Test Title", result.getTitle());
+                assertEquals("Test Description", result.getDescription());
+                assertEquals("Test Content", result.getContent());
+                assertEquals("Test Image URL", result.getImageUrl());
+                assertEquals(1L, result.getId());
+                assertEquals(BlogStatus.DRAFT, result.getBlogStatus());
+                assertNotNull(result.getCreatedAt());
+                assertEquals(PostConstents.SYSTEM_USER, result.getCreatedBy());
+                assertEquals(BlogStatus.DRAFT, result.getBlogStatus());
+                // Verify
+                verify(blogRepository, times(1)).findById(1L);
+                verify(blogRepository, times(1)).save(any(BlogPost.class));
+                verifyNoMoreInteractions(blogRepository);
+        }
+
+        @Test
+        void testUpdateBlog_WithNotFoundException() {
+                // Mock
+                when(blogRepository.findById(anyLong())).thenReturn(Optional.empty());
+                // Arrange
+                BlogPostRequest existingTitleRequest = BlogPostRequest.builder().id(1L).title("Existing Title")
+                                .content("Existing Content").build();
+
+                // Act & Assert
+                assertThrows(NotFoundException.class, () -> service.updateBlog(1L, existingTitleRequest),
+                                "Blog Post not found may be unpublished id 1");
+                // Verify
+                verify(blogRepository, times(1)).findById(1L);
+                verifyNoMoreInteractions(blogRepository);
+        }
+
+        @Test
+        void testUpdateBlog_DataIntegrityViolationException() {
+                // Arrange
+                BlogPostRequest validRequest = BlogPostRequest.builder().title("New Title").content("New Content")
+                                .build();
+                when(blogRepository.findById(anyLong())).thenReturn(Optional.of(BlogPost.builder().build()));
+                when(blogRepository.save(any(BlogPost.class))).thenThrow(DataIntegrityViolationException.class);
+
+                // Act & Assert
+                assertThrows(CustomDataIntegrityViolationException.class, () -> service.updateBlog(1L, validRequest));
+                // Verify
+                verify(blogRepository, times(1)).findById(1L);
+                verify(blogRepository, times(1)).save(any(BlogPost.class));
+                verifyNoMoreInteractions(blogRepository);
+
+        }
+
+        /*
+         * ! ==========> Test for deleteBlog() <==========
+         */
+
+        @Test
+        void testDeleteBlog_BlogExists_DeletesSuccessfully() {
+                Long existingBlogId = 1L;
+                BlogPost existingBlog = new BlogPost();
+                existingBlog.setId(existingBlogId);
+
+                when(blogRepository.findById(existingBlogId)).thenReturn(Optional.of(existingBlog));
+
+                service.deleteBlog(existingBlogId);
+
+                verify(blogRepository, Mockito.times(1)).delete(existingBlog);
+                verifyNoMoreInteractions(blogRepository);
+        }
+
+        @Test
+        void testDeleteBlog_BlogDoesNotExist_NoDeletion() {
+                Long nonExistingBlogId = 2L;
+
+                when(blogRepository.findById(nonExistingBlogId)).thenReturn(Optional.empty());
+
+                service.deleteBlog(nonExistingBlogId);
+
+                verify(blogRepository, Mockito.never()).delete(Mockito.any());
+                verifyNoMoreInteractions(blogRepository);
+        }
 }
